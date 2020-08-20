7e6edb9a7f849a984a2cea576eed15e3f9acd350
MatthieuMuratore <matthieu.muratore@gmail.com>
2020-08-20 11:41:18 +0200

    create AuthRequired Attribute for Api

---
+++ GoReal.Api/Infrastrucutre/AuthRequiredAttribute.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Tools.Security.Token;
+
+namespace GoReal.Api.Infrastrucutre
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthRequiredAttribute : TypeFilterAttribute
+    {
+        public AuthRequiredAttribute() : base(typeof(AuthRequiredFilter)) {}
+    }
+
+    public class AuthRequiredFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            ITokenService _tokenService = (ITokenService)context.HttpContext.RequestServices.GetService(typeof(ITokenService));
+
+            //TODO Find a way to get actionContext
+            context.HttpContext.Request.Headers.TryGetValue("Authorization", out StringValues authorizations);
+
+            string token = authorizations.SingleOrDefault(t => t.StartsWith("Bearer "));
+
+            if (token is null)
+            {
+                context.Result = new StatusCodeResult((int)HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                IEnumerable<string> properties = new List<string>() { "UserId", "LastName", "FirstName", "Email" };
+                IDictionary<string, string> user = _tokenService.DecodeToken(token, properties);
+
+                if (user is null)
+                {
+                    context.Result = new StatusCodeResult((int)HttpStatusCode.Unauthorized);
+                }
+                else
+                {
+                    context.RouteData.Values.Add("userId", user["UserId"]);
+                }
+            }
+
+        }
+    }
+}
